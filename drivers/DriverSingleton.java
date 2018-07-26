b2b9af002b4db8182173710645acf3fe04dc80a9
unknown <sashaxt@hotmail.com>
2018-07-26 17:30:25 +0300

    Seems driver works

--- drivers/DriverSingleton.java
+++ drivers/DriverSingleton.java
@@ -1,13 +1,21 @@
 import org.openqa.selenium.WebDriver;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
 import java.util.concurrent.TimeUnit;
 
 public class DriverSingleton {
 
-    private static String CHROME_DRIVER = "webdriver.chrome.driver";
-    private static String CHROME_PATH = ".\\WebDriver\\chromedriver_win32\\chromedriver.exe";
-    private static int PAGE_LOAD_TIMEOUT = 60;
-    private static int DEFAULT_IMPLICITLY = 10;
+    private static final String CHROME_DRIVER = "webdriver.chrome.driver";
+    private static final String CHROME_PATH = ".\\WebDriver\\chromedriver_win32\\chromedriver.exe";
+    private static final String NORMAL_PARAMETER = "normal";
+    private static final int PAGE_LOAD_TIMEOUT = 60;
+    private static final int DEFAULT_IMPLICITLY = 10;
 
     public static WebDriver driver;
 
@@ -15,6 +23,10 @@ public class DriverSingleton {
         if (driver == null)
         {
             System.setProperty(CHROME_DRIVER, CHROME_PATH);
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("disable-infobars");
+            options.addArguments(CapabilityType.PAGE_LOAD_STRATEGY, NORMAL_PARAMETER);
+            driver = new ChromeDriver(options);
             driver.manage().timeouts().pageLoadTimeout(PAGE_LOAD_TIMEOUT, TimeUnit.SECONDS);
             driver.manage().timeouts().implicitlyWait(DEFAULT_IMPLICITLY, TimeUnit.SECONDS);
             driver.manage().window().maximize();
--- tests/TestCase1.java
+++ tests/TestCase1.java
@@ -1,6 +1,9 @@
+import org.testng.annotations.Test;
+
 public class TestCase1 {
 
     //run the app
+    @Test
     public static void OpenSite() {
         DriverSingleton.getDriver();
         DriverSingleton.driver.navigate().to("https://www.skyscanner.com");
